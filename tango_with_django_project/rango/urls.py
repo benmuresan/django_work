8274d961bcd5a780151bd415525103797202083e
Beniamin Muresan <benmuresan@yahoo.com>
2015-04-08 21:23:02 -0700

    Updated rango

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -8,7 +8,9 @@ if not settings.DEBUG:
         urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-        url(r'^about/$', views.about, name='about'), # New about line.
-
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+    url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!
 )
